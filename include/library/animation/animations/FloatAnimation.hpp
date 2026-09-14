c18525cadd22de86cfb9afe4a418cd98f874c435
Oschdi <osterjakob@gmail.com>
2026-09-14 22:56:18 +0200

    Animations

---
+++ include/library/animation/animations/FloatAnimation.hpp
@@ -0,0 +1,18 @@
+#ifndef EDNA_ENGINE_FLOATANIMATION_HPP
+#define EDNA_ENGINE_FLOATANIMATION_HPP
+
+#include <library/animation/animations/Animation.hpp>
+
+namespace Animation
+{
+    class FloatAnimation : public Animation<float>
+    {
+    public:
+        using Animation<float>::Animation;
+
+    protected:
+        float interpolate(float t) const override;
+    };
+}
+
+#endif //EDNA_ENGINE_FLOATANIMATION_HPP
