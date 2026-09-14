f5c27a3273c76eded1b074e5c9d3368cd67ca1e4
Oschdi <osterjakob@gmail.com>
2026-09-14 23:09:13 +0200

    Animation Curves

---
+++ include/library/animation/curves/AnimationCurve.hpp
@@ -0,0 +1,57 @@
+#ifndef EDNA_ENGINE_ANIMATIONCURVE_HPP
+#define EDNA_ENGINE_ANIMATIONCURVE_HPP
+
+#include <functional>
+#include <memory>
+#include <type_traits>
+#include <vector>
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+#include <library/animation/animations/Animation.hpp>
+#include <library/animation/animations/FloatAnimation.hpp>
+#include <library/animation/animations/IntAnimation.hpp>
+#include <library/animation/animations/VectorAnimation.hpp>
+#include <library/animation/easing_functions/EasingCurve.hpp>
+#include <library/animation/easing_functions/EasingDirection.hpp>
+#include <library/animation/easing_functions/EasingFunction.hpp>
+
+namespace Animation
+{
+    template <typename T, typename AnimT>
+    class AnimationCurve
+    {
+    public:
+        static_assert(std::is_base_of_v<Animation<T>, AnimT>,
+                      "AnimT must derive from Animation<T>");
+
+        struct FixPoint
+        {
+            T value{};
+            int steps = 1;
+            EasingCurve curve = EasingCurve::Linear;
+            EasingDirection direction = EasingDirection::InOut;
+            std::shared_ptr<EasingFunction> easing_override;
+        };
+
+        AnimationCurve& add(FixPoint point);
+        AnimationCurve& onUpdate(std::function<void(const T&)> on_update);
+
+        std::vector<std::unique_ptr<Animation<T>>> build() const;
+
+    private:
+        std::vector<FixPoint> points;
+        std::function<void(const T&)> on_update;
+    };
+
+    using FloatAnimationCurve = AnimationCurve<float, FloatAnimation>;
+    using IntAnimationCurve = AnimationCurve<int, IntAnimation>;
+
+    using Vec2AnimationCurve = AnimationCurve<glm::vec2, VectorAnimation<glm::vec2>>;
+    using Vec3AnimationCurve = AnimationCurve<glm::vec3, VectorAnimation<glm::vec3>>;
+    using Vec4AnimationCurve = AnimationCurve<glm::vec4, VectorAnimation<glm::vec4>>;
+}
+
+#endif //EDNA_ENGINE_ANIMATIONCURVE_HPP
---
+++ src/library/animation/curves/AnimationCurve.cpp
@@ -0,0 +1,55 @@
+#include <library/animation/curves/AnimationCurve.hpp>
+#include <library/animation/easing_functions/EasingFunctionFactory.hpp>
+
+#include <utility>
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+
+namespace Animation
+{
+    template <typename T, typename AnimT>
+    AnimationCurve<T, AnimT>& AnimationCurve<T, AnimT>::add(FixPoint point)
+    {
+        points.push_back(std::move(point));
+        return *this;
+    }
+
+    template <typename T, typename AnimT>
+    AnimationCurve<T, AnimT>& AnimationCurve<T, AnimT>::onUpdate(std::function<void(const T&)> on_update)
+    {
+        this->on_update = std::move(on_update);
+        return *this;
+    }
+
+    template <typename T, typename AnimT>
+    std::vector<std::unique_ptr<Animation<T>>> AnimationCurve<T, AnimT>::build() const
+    {
+        std::vector<std::unique_ptr<Animation<T>>> segments;
+        if (points.size() < 2)
+            return segments;
+
+        segments.reserve(points.size() - 1);
+        for (size_t i = 1; i < points.size(); ++i)
+        {
+            const auto& prev = points[i - 1];
+            const auto& cur = points[i];
+
+            auto easing = cur.easing_override
+                              ? cur.easing_override
+                              : makeEasingFunction(cur.curve, cur.direction);
+
+            segments.emplace_back(std::make_unique<AnimT>(
+                prev.value, cur.value, cur.steps, on_update, std::move(easing)));
+        }
+        return segments;
+    }
+
+    template class AnimationCurve<float, FloatAnimation>;
+    template class AnimationCurve<int, IntAnimation>;
+    template class AnimationCurve<glm::vec2, VectorAnimation<glm::vec2>>;
+    template class AnimationCurve<glm::vec3, VectorAnimation<glm::vec3>>;
+    template class AnimationCurve<glm::vec4, VectorAnimation<glm::vec4>>;
+}
